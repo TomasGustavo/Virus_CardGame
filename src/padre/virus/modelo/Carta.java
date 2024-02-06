a6b6a143a50a6c99dd52457d4287641d6f366000
TomasGustavo <101200943+ImKyrio@users.noreply.github.com>
2024-02-05 22:59:52 -0300

    - Cmabia a clase abstracta

--- src/padre/virus/modelo/Carta.java
+++ src/padre/virus/modelo/Carta.java
@@ -1,50 +1,25 @@
 package padre.virus.modelo;
 
+import java.io.Serializable;
 
-import java.awt.*;
+public abstract class Carta implements ICarta, Serializable {
 
-public class Carta implements ICarta {
-    private Color color;
-    private Tipo tipo;
-    private Image imagen;
+    protected Color color;
+    protected Tipo tipo;
 
-
-    public Carta (Color color, Tipo tipo){
+    public Carta (Color color,Tipo tipo){
         this.color = color;
         this.tipo = tipo;
     }
+
     public Color getColor(){
         return color;
     }
     public Tipo getTipo(){
         return tipo;
     }
-
-    @Override
-    public String getImage() {
-        return null;
-    }
-
-    @Override
     public String toString(){
-        return "Carta [color= "+color+", tipo= "+tipo+"]";
+        return color.toString()+" "+ tipo;
     }
-    public void setColor(Color color){
-        this.color = color;
-    }
-    public void setTipo(Tipo tipo){
-        this.tipo = tipo;
-    }
-    /*
-    public Image getImagen(){
-        String color = getColor().toString().toLowerCase();
-        String tipo = getTipo().toString().toLowerCase();
 
-        imagen = new Image ("/image/carta/"+color+tipo+".png");
-        return imagen;
-    }*/
-
-    public void setImagen(Image imagen){
-        this.imagen = imagen;
-    }
 }
